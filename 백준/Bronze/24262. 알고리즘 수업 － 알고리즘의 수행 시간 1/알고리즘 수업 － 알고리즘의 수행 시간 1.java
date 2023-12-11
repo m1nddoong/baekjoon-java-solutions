c65d21de1a1a50299bf4f6f1f2a0adce1ec37fdd
SEON IK MIN <110027583+m1nddoong@users.noreply.github.com>
2023-12-11 17:12:52 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 120 ms, Memory: 14044 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+
+// 알고리즘의 수행 시간 1 : https://www.acmicpc.net/problem/24262
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.println(1);
+        System.out.println(0);
+    }
+}
