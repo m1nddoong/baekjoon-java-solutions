d0a474bb83bb9d2faa146688c7c1f795783924fb
SEON IK MIN <110027583+m1nddoong@users.noreply.github.com>
2023-12-21 20:19:33 +0900

    [Silver V] Title: 다리 놓기, Time: 168 ms, Memory: 16068 KB -BaekjoonHub

---
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+        int T = Integer.parseInt(reader.readLine());
+        for (int i = 0; i < T; i++) {
+            String input = reader.readLine();
+            int n = Integer.parseInt(input.split(" ")[1]);
+            int r = Integer.parseInt(input.split(" ")[0]);
+            dp = new int[n+1][r+1];
+            writer.write(BC(n, r) + "\n");
+        }
+        writer.flush();
+    }
+
+    // private static int[][] dp = new int[30][30]; 메모리 할당 문제
+    static int[][] dp;
+
+    public static int BC(int n, int r) { // 이항 계수(조합)
+        if (dp[n][r] > 0) {
+            return dp[n][r];
+        }
+        if (n == r || r == 0) {
+            //return dp[n][r] = 1;
+            return 1;
+        }
+        // return BC(n - 1, r - 1) + BC(n - 1, r);
+        int result = BC(n - 1, r - 1) + BC(n - 1, r);
+        dp[n][r] = result;
+        return result;
+    }
+}
+
