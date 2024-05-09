0a22741fbc5d9dce361a9538a66b255f53daca60
IKSUN <110027583+m1nddoong@users.noreply.github.com>
2024-05-09 20:47:15 +0900

    [Bronze I] Title: 세로읽기, Time: 124 ms, Memory: 14220 KB -BaekjoonHub

--- 백준/Bronze/10798. 세로읽기/세로읽기.java
+++ 백준/Bronze/10798. 세로읽기/세로읽기.java
@@ -6,26 +6,26 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[][] matrix = new String[5][15]; // 5행 15열
+        char[][] matrix = new char[5][15]; // 5행 15열
         for (int i = 0; i < 5; i++) {
             String str = br.readLine();
             char[] charArray = str.toCharArray();
             for (int j = 0; j < charArray.length; j++) {
-                matrix[i][j] = String.valueOf(charArray[j]);
+                matrix[i][j] = charArray[j];
             }
         }
         verticalRead(matrix);
 
     }
 
-    public static void verticalRead(String[][] matrix) {
+    public static void verticalRead(char[][] matrix) {
         StringBuilder sb = new StringBuilder();
         // 0행0열 1행0열 2행0열 ...
         for (int i = 0; i < 15; i++) {
             for (int j = 0; j < 5; j++) {
-                String s = matrix[j][i];
-                if (s != null) {
-                    sb.append(s);
+                char c = matrix[j][i];
+                if (c != '\0') {
+                    sb.append(c);
                 }
             }
         }
